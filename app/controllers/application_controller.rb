47caf616efb671072ee8b5231b45ba96389892f0
aliceguo2016reborn <aliceguo2016reborn@gmail.com>
2017-01-26 13:24:58 +0800

    seeds

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
-  def admin_required
-    if !current_user.email.admin?
-      redirect_to root_path
-      flash[:notice] = "you are not admin!"
-    end
-  end
+   def admin_required
+     if !current_user.admin?
+       redirect_to "/"
+     end
+   end
 
 end
